b8b0aa1665dade346abea1a76d3b853fe9942da5
lingxiaozhang <1614774556@qq.com>
2026-02-26 10:40:06 +0800

    1

---
+++ sso-auth/src/main/java/com/zhanglx/sso/auth/config/MyBatisConfig.java
@@ -0,0 +1,30 @@
+package com.zhanglx.sso.auth.config;
+
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+/**
+ * @Author: Zhang L X
+ * @Create: 2026/2/26 10:35
+ * @ClassName: MyBatisConfig
+ * @Description:
+ */
+@Configuration
+@MapperScan("com.zhanglx.sso.auth.mapper")
+public class MyBatisConfig {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        MybatisSqlSessionFactoryBean bean = new MybatisSqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:mapper/*.xml"));
+        return bean.getObject();
+    }
+
+}
