8cc6df60b2b3b31d1b14172e28937cb4f338ec6a
lingxiaozhang <1614774556@qq.com>
2026-04-08 13:21:35 +0800

    新增日志模块

---
+++ sso-log/src/main/java/com/zhanglx/sso/log/LogAutoConfig.java
@@ -0,0 +1,13 @@
+package com.zhanglx.sso.log;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: Zhang L X
+ * @Create: 2026/2/26 15:46
+ * @ClassName: MybatisAutoConfig
+ * @Description:
+ */
+@Configuration
+public class LogAutoConfig {
+}
